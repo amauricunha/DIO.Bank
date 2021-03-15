9e06916d4c66a7f80683da8ea8a2dcd541811b65
Amauri Cunha <proamauri@gmail.com>
2021-03-15 09:56:16 -0300

    Commit inicial + parte 2

---
+++ DIO.Bank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DIO.Bank.Model;
+using DIO.Bank.Enum;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
