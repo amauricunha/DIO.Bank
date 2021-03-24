ef51d4a7a4d38a32271fb428bfcbddd727e1e5aa
Amauri Cunha <proamauri@gmail.com>
2021-03-23 22:14:30 -0300

    Metodos da Classe + Estrutura Principal do Program

--- DIO.Bank/Model/Conta.cs
+++ DIO.Bank/Model/Conta.cs
@@ -1,3 +1,4 @@
+using System;
 using DIO.Bank.Enum;
 
 namespace DIO.Bank.Model
@@ -9,11 +10,47 @@ namespace DIO.Bank.Model
         private double Credito { get; set; }
         private string Nome { get; set; }
 
+        //Método construtor: chamado no momento que é criado o objeto.
         public Conta (TipoConta tipoConta, double saldo, double credito, string nome){
+            //this é para alterar a instância chamado, e não todas elas.
             this.TipoConta = tipoConta;
             this.Saldo = saldo;
             this.Credito = credito;
             this.Nome = nome;
-         }        
+         }
+
+         public bool Sacar (double valorSaque){
+             if ((this.Saldo - valorSaque) < (this.Credito*-1))
+             {
+                 Console.WriteLine("Saldo insuficiente");
+                 return false;
+             }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual de {0} é {1}",this.Nome, this.Saldo); //formatação composta
+            return true;
+         }
+
+         public void Depositar (double valorDeposito) {
+             this.Saldo += valorDeposito;
+             Console.WriteLine("Depósito realizado com sucesso");
+             Console.WriteLine("Saldo atual de {0} é {1}",this.Nome, this.Saldo);
+         }
+
+         public void Transferir (double valorTransferencia, Conta contaDestino) {
+             if (this.Sacar(valorTransferencia)) //já tenho validação neste método, não é necessário retornar bool
+             {
+                 contaDestino.Depositar(valorTransferencia); //também já tenho retorno descritivo neste método.
+             }
+         }
+
+        public override string ToString() //sobrescrevendo o método padrão ToString. Muito usado para logs.
+        {
+            string retorno = "";
+            retorno += "Tipo Conta: " + this.TipoConta + " | ";
+            retorno += "Nome: " + this.Nome + " | ";
+            retorno += "Saldo: " + this.Saldo + " | ";
+            retorno += "Crédito: " + this.Credito;
+            return retorno;
+        }
     }
 }
--- DIO.Bank/Program.cs
+++ DIO.Bank/Program.cs
@@ -1,14 +1,106 @@
 ﻿using System;
 using DIO.Bank.Model;
 using DIO.Bank.Enum;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listaContas = new List<Conta>(); //Collection
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Conta minhaConta = new Conta(TipoConta.PessoaFisica,100.00,50.00,"Amauri");
+            //Console.WriteLine(minhaConta.ToString());
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario) {
+                    case "1":
+                    ListarContas();
+                    break;
+                    case "2":
+                    InserirConta();
+                    break;
+                    case "3":
+                    //Transferir();
+                    break;
+                    case "4":
+                    //Sacar();
+                    break;
+                    case "5":
+                    //Depositar();
+                    break;
+                    case "C":
+                    Console.Clear();
+                    break;
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+
         }
+
+       
+        private static void InserirConta() {
+            Console.WriteLine("Inserir nova conta:");
+            int entradaTipoConta = 0;
+            bool validaTipoConta = false;
+            while (!validaTipoConta)
+            {
+                Console.WriteLine("Digite o tipo de conta: 1 para Física e 2 para Jurídica");
+                string entradaTipoContaString = Console.ReadLine();
+                if (entradaTipoContaString == "1") 
+                {
+                    entradaTipoConta = int.Parse(entradaTipoContaString);
+                    validaTipoConta = true;
+                } else if (entradaTipoContaString == "2") {
+                    entradaTipoConta = int.Parse(entradaTipoContaString);
+                    validaTipoConta = true;
+                } else {
+                    Console.WriteLine("Tipo de Conta Inválido, escolha 1 para Física e 2 para Jurídica");
+                }
+            }
+            Console.WriteLine("Digite o nome da conta:");
+            string entradaNomeConta = Console.ReadLine();
+            Console.WriteLine("Digite o saldo inicial da conta:");
+            double entradaSaldoConta = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o crédito inicial da conta:");
+            double entradaCreditoConta = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta:(TipoConta) entradaTipoConta
+                                        ,entradaSaldoConta
+                                        ,entradaCreditoConta
+                                        ,entradaNomeConta);
+
+            listaContas.Add(novaConta);
+        }
+
+        private static void ListarContas()
+        {
+            foreach (Conta conta in listaContas){
+                Console.WriteLine(conta.ToString());
+            }
+        }
+
+        private static string ObterOpcaoUsuario() {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank ao seu dispor!");
+            Console.WriteLine("1 - Lista Contas");
+            Console.WriteLine("2 - Inserir Conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            return opcaoUsuario;
+        }
+
+
     }
 }
